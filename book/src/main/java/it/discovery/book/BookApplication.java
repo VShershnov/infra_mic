9aa7b90f6be93e0dca1f823216b0759b8191f284
Sergey <sergey.morenets@yandex.ua>
2018-12-02 12:59:28 +0200

    Task #13 - Clean up Ribbon configuration

--- book/src/main/java/it/discovery/book/BookApplication.java
+++ book/src/main/java/it/discovery/book/BookApplication.java
@@ -3,24 +3,14 @@ package it.discovery.book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@RibbonClient("statistics")
 @EnableFeignClients
 public class BookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
-    }
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate statisticsRestTemplate() {
-        return new RestTemplate();
     }
 }
